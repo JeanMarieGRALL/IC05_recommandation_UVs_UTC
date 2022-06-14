17f64a1501d141be288fe07d7c8d5903ef39aaf7
bdommart <brieuc.dommartin@etu.utc.fr>
2022-06-14 18:23:22 +0200

    algo recommendation analyse textuelle

---
+++ code/data_scrapping/infos_UV_export_LDA.R
@@ -0,0 +1,44 @@
+library(stringr)
+library(readr)
+library(dplyr)
+
+setwd("C:/Users/brieu/Desktop/Cours/IC05/IC05_projet/R/ScrapENT")
+
+UVs <- read_csv("../../data/UVs.csv")
+
+
+description = UVs[,c("Code",
+                     "Mots_cles",
+                     "Description",
+                     "Intitule",
+                     "Domaine",
+                     "Theme",
+                     "Objectif_formation",
+                     "Objectifs_pedagogiques_specifiques",
+                     "Objectifs_pedagogiques_transverses",
+                     "Programme", "Resulat"
+)]
+
+description$Description = str_replace_all(description$Description,"Ã©","e")
+description$Description = str_replace_all(description$Description,"�","c")
+description$Description = str_replace_all(description$Description,"Ãš","e")
+description$Description = str_replace_all(description$Description,"A Ã"," a")
+
+description = description[description$Code != "SC23",]
+
+description$infos = paste(description$Intitule,
+                          description$Mots_cles, 
+                          description$Description, 
+                          description$Domaine,
+                          description$Theme,
+                          description$Objectif_formation,
+                          description$Objectifs_pedagogiques_specifiques,
+                          description$Objectifs_pedagogiques_transverses,
+                          description$Programme,
+                          description$Resulat,
+                          sep = " / ")
+
+description = distinct(description, Code, .keep_all = T )
+
+write.csv(description[,c("Code","infos")], "descriptions.csv")
+
